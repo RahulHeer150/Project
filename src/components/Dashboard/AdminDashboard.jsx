1f04dd3aa1c6d8cbab9cffde5f693f193c3c041e
Rahul Heer <rahulheer344@gmail.com>
2025-08-15 19:57:22 +0530

    new files

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../others/Header'
 import CreateTask from '../others/CreateTask'
+import AllTask from '../others/AllTask'
 
 const AdminDashboard = () => {
   return (
     <div className='h-screen p-10 w-full'>
         <Header/>
         <CreateTask/>
+        <AllTask/>
 
       
     </div>
